b463d608e456772a7522e137852a487a0a7b1aa3
Ujwal Kashyap <usualketchup@berkeley.edu>
2020-05-04 13:13:02 -0700

    updated folders

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './style.css';
+
 const Navbar = () => (
   <div>
-    
+    <Link to='/home'>Home</Link>
+    <Link to='/trending'>Trending</Link>
+    <Link to='/dashboard'>Dashboard</Link>
+    <Link to='/signup'>Signup/Login</Link>
   </div>
 );
 
---
+++ client/src/containers/Dashboard/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import './style.css';
+
+class Dashboard extends Component {
+  state = {
+    dash: []
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>I'm in Dashboard!</h1>
+      </div> 
+    )
+  };
+}
+
+export default Dashboard;
