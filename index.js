2e4491caa396e9f2e85aeb95d4f0740338f9ad28
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-10 23:30:39 -0700

    fixed heroku route

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', '/public/index.html'));
+  });
 }
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', '/public/index.html'));
-});
 
 app.use(routes);
 
