eca783b9eb03f02b3cc1838900c1bdcd6335c634
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-10 22:55:56 -0700

    made /public/index.html route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
@@ -10,6 +11,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.use(routes);
 
