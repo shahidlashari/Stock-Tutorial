b1ed24d169f7c74cffddfffe45f14ce1e9a6b531
Norberto Mantohac <nmantohac82098@gmail.com>
2020-05-11 09:19:42 -0700

    updated heroku route

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', '/public/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'public/index.html'));
   });
 }
 
